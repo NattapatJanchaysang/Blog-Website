4de72b8c4204080ca8604f4c2274d779701c57b7
NattapatJanchaysang <66011360@kmitl.ac.th>
2025-12-04 02:16:18 +0700

    Styling Home page

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -3,40 +3,45 @@ import { Link } from 'react-router-dom';
 
 export const Home = () => {
 
-  let posts = [
-    {
-      id: "1",
-      "img":"Logo1.png",
-      "title":"What is Lorem Ipsum?",
-      "desc":"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
-    },
-        {
-      id: "2",
-      "img":"Logo2.png",
-      "title":"What is Lorem Ipsum?",
-      "desc":"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
-    },
-            {
-      id: "3",
-      "img":"Logo2.png",
-      "title":"What is Lorem Ipsum?",
-      "desc":"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
-    },
-  ]
-
+   const posts = [
+     {
+       id: 1,
+       title: "Lorem ipsum dolor sit amet consectetur adipisicing elit",
+       desc: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. A possimus excepturi aliquid nihil cumque ipsam facere aperiam at! Ea dolorem ratione sit debitis deserunt repellendus numquam ab vel perspiciatis corporis!",
+       img: "https://images.pexels.com/photos/7008010/pexels-photo-7008010.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+     },
+     {
+       id: 2,
+       title: "Lorem ipsum dolor sit amet consectetur adipisicing elit",
+       desc: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. A possimus excepturi aliquid nihil cumque ipsam facere aperiam at! Ea dolorem ratione sit debitis deserunt repellendus numquam ab vel perspiciatis corporis!",
+       img: "https://images.pexels.com/photos/6489663/pexels-photo-6489663.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+     },
+     {
+       id: 3,
+       title: "Lorem ipsum dolor sit amet consectetur adipisicing elit",
+       desc: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. A possimus excepturi aliquid nihil cumque ipsam facere aperiam at! Ea dolorem ratione sit debitis deserunt repellendus numquam ab vel perspiciatis corporis!",
+       img: "https://images.pexels.com/photos/4230630/pexels-photo-4230630.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+     },
+     {
+       id: 4,
+       title: "Lorem ipsum dolor sit amet consectetur adipisicing elit",
+       desc: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. A possimus excepturi aliquid nihil cumque ipsam facere aperiam at! Ea dolorem ratione sit debitis deserunt repellendus numquam ab vel perspiciatis corporis!",
+       img: "https://images.pexels.com/photos/6157049/pexels-photo-6157049.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+     },
+   ];
 return (
     <div>
       <div>
         {posts.map((post) => (
-          <div key={post.id} className='flex items-center container pt-20'>
+          <div key={post.id} className='md:flex items-center container pt-20 md:[&:nth-child(odd)]:flex-row-reverse'>
             <div>
-              <img className='container h-auto' src={`./public/${post.img}`} alt="" />
+              <img className='w-auto max-h-200 px-24 ' src={`${post.img}`} alt="" />
             </div>
             <div className="flex flex-col">
                 <h1 className='text-center font-bold text-2xl font-mono text-glow'>{post.title}</h1>
               <p className='container p-4 font-mono text-justify'>{(post.desc)}</p>
               <Link className="mx-auto" to={`/post/${post.id}`}>
-              <button className='main-button mx-auto bg-yellow-200 text-black'>Read More</button>
+              <button className='main-button mx-auto bg-yellow-200 text-black cursor-pointer'>Read More</button>
               </Link>
             </div>
           </div>
